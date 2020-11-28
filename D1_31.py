ed4cc946b75fa35ea107c5a435ac5546bc8f0abe
SaravanaB03 <75078087+SaravanaB03@users.noreply.github.com>
2020-11-28 11:54:33 +0530

    Add files via upload

---
+++ D1_31.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print('Hello world welcome to python')
+
+
+# In[1]:
+
+
+print(Hello world welcome to python)
+
+
+# In[2]:
+
+
+print(saravana)
+
+
+# In[ ]:
+
+
+
+
